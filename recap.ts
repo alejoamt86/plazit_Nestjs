a31ba9335c1ed74f4f4fc59914b7c3287fb73569
Alejandro Mejia <54806912+alejoamt86@users.noreply.github.com>
2022-10-06 11:22:27 -0500

    typescript review

--- recap.ts
+++ recap.ts
@@ -7,3 +7,15 @@ const suma = (a: number, b: number) => {
     return a + b;
 }
 suma(12, 12);
+
+class Persona{
+    constructor(private age:number, private name:string){
+        this.age=age;
+        this.name= name;
+    }
+    getSummary(){
+        return `my name is ${this.name}, and my age is ${this.age}`;
+    }
+}
+
+const Alajandro = new Persona(36,'Alajandro');
