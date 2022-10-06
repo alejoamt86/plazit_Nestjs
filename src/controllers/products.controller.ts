c89fc35978b1a9be7bb07200845c4aef045ebba0
Alejandro Mejia <54806912+alejoamt86@users.noreply.github.com>
2022-10-06 17:29:23 -0500

    Status codes or HTTP response status codes

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Query } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -23,36 +23,37 @@ export class ProductsController {
     }
 
     @Post()
-    create(@Body() payload:any) {
-        return { message: 'accion de crear', payload};
+    @HttpCode(HttpStatus.ACCEPTED)
+    create(@Body() payload: any) {
+        return { message: 'accion de crear', payload };
     }
 
     @Put(':id')
-    update(@Param('id') productId:number, @Body() payload:any){
-        return { 
-            message: 'accion de actualizar', 
+    update(@Param('id') productId: number, @Body() payload: any) {
+        return {
+            message: 'accion de actualizar',
             productId,
             payload
         };
     }
 
     @Put('')
-    updateMax(@Body() payload:any){
-        return { 
-            message: 'accion de actualizar', 
+    updateMax(@Body() payload: any) {
+        return {
+            message: 'accion de actualizar',
             payload
         };
     }
 
     @Delete(':id')
-    delecte(@Param('id') id: number){
-        return {messge:'Accion de eliminar un solo elemento',id}
+    delecte(@Param('id') id: number) {
+        return { messge: 'Accion de eliminar un solo elemento', id }
     }
 
     @Delete()
-    delecteMax(@Body() payload:any){
-        return {messge:'Accion de eliminar maxivamente',payload}
+    delecteMax(@Body() payload: any) {
+        return { messge: 'Accion de eliminar maxivamente', payload }
     }
-    
+
 
 }
