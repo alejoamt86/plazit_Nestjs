f4926991af254d711caf4f96485250c8c12b59cd
Alejandro Mejia <54806912+alejoamt86@users.noreply.github.com>
2022-10-06 11:33:48 -0500

    controllers new endpoint with decorators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+ 
+  // http://localhost:3000
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // http://localhost:3000/new
+  @Get('new')
+  getNewEndpoint():string{
+    return 'hola yo soy un nuevo endpoint';
   }
 }
